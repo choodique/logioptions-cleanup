b00cb1b95a9677b935f7ed3746b415bfa27a05f3
choodique <choodique@pm.me>
2021-10-14 00:55:53 +0300

    First commit

---
+++ logioption-cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf /Applications/logioptionsplus.app
+rm -rf /Library/Application\ Support/Logitech.localized/LogiOptionsPlus
+rm -rf /Library/LaunchAgents/com.logi.optionsplus.plist
+rm -rf /Library/LaunchDaemons/com.logi.optionsplus.updater.plist
+rm -rf /Users/Shared/LogiOptionsPlus
+rm -rf ~/Library/Application\ Support/LogiOptionsPlus
+rm -rf ~/Library/Caches/com.logi.optionsplus.installer
+rm -rf ~/Library/Preferences/com.logi.optionsplus.plist
+rm -rf ~/Library/Saved\ Application\ State/com.logi.optionsplus.savedState
+rm -rf ~/Library/Application\ Support/LogiOptionsPlus
